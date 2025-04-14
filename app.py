301352a210392424bb11f3c8a020499af5f80be2
smookymolina <164466092+smookymolina@users.noreply.github.com>
2025-04-14 16:12:44 -0600

    Update and rename app.js to app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Recluta
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/reclutas', methods=['GET'])
+def get_reclutas():
+    reclutas = Recluta.query.all()
+    return jsonify([r.serialize() for r in reclutas])
+
+@app.route('/api/reclutas', methods=['POST'])
+def add_recluta():
+    data = request.get_json()
+    nuevo = Recluta(
+        nombre=data['nombre'],
+        email=data['email'],
+        telefono=data['telefono'],
+        estado=data['estado']
+    )
+    db.session.add(nuevo)
+    db.session.commit()
+    return jsonify(nuevo.serialize()), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
