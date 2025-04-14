5ca6b697a1b95dd0a820423e4b2d2d52de9d21c2
smookymolina <164466092+smookymolina@users.noreply.github.com>
2025-04-14 16:15:06 -0600

    Update and rename styles.css to models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Recluta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    telefono = db.Column(db.String(20))
+    estado = db.Column(db.String(20))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'email': self.email,
+            'telefono': self.telefono,
+            'estado': self.estado
+        }
